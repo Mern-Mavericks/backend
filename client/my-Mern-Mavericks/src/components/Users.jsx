4880813a038f02e1b41c67892fcfbdc08013adff
fullstackkg <khalilhgayle@gmail.com>
2024-08-01 04:00:11 -0400

    Updated Users component

--- client/my-Mern-Mavericks/src/components/Users.jsx
+++ client/my-Mern-Mavericks/src/components/Users.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
+import { getAllUsers } from "../../../../../api/authApi";
 
 const Users = () => {
-  return <div>All Users Page</div>;
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      try {
+        const result = await getAllUsers();
+        if (result.error) {
+          setError(result.error);
+        } else {
+          setUsers(result);
+        }
+      } catch (err) {
+        setError("Failed to fetch users");
+      }
+    };
+    fetchAllUsers();
+  }, []);
+
+  const displayErrorMessage = () => {
+    if (error) {
+      return <p>{error}</p>;
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      <h2>All Users</h2>
+      {displayErrorMessage()}
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Users;
